0d771320ed7d2ad610de56574cc3d808ce21bd4f
Bowuigi <70549864+Bowuigi@users.noreply.github.com>
2020-11-12 17:13:34 -0300

    Added Pages Integration

---
+++ main.js
@@ -0,0 +1,8 @@
+function rndteleport() {
+  document.getElementById('no').style.left=String(Math.floor(Math.random()*screen.width))+'px';
+  document.getElementById('no').style.top=String(Math.floor(Math.random()*screen.height))+'px';
+}
+
+function yay(){
+  alert('Sabia que ibas a decir que si jijiji');
+}
